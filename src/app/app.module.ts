d7f210811b752c34f949a5ed1b84488bb20903dd
Tore Aurstad <tore.aurstad@hemit.no>
2019-10-20 00:21:43 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { EnumselectioncontrolsModule } from "enumselectioncontrols";
+import { RadioButtonListComponent } from 'enumselectioncontrols';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent ],
+  imports: [BrowserModule, EnumselectioncontrolsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/ice-cream.ts
@@ -0,0 +1,6 @@
+export enum IceCream {
+  None = 0,
+  Vanilla = 1,
+  Strawberry = 2,
+  Peach = 3
+}
